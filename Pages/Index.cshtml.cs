e734df16515d47a88994ea10bd004f1d847bef0f
jacks <jacks@LUMINOECHO>
2025-09-30 14:09:41 +0200

    This is the first view of the template Razer pages of this individual project

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Individual_Project_2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
