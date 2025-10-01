8b4ebea625eeda13f32c34705647ebf9995f2fa2
jacks <jacks@LUMINOECHO>
2025-10-01 11:11:05 +0200

    Modified the gitignore file using a different template

--- obj/Release/net10.0/Individual_Project_2.AssemblyInfo.cs
+++ obj/Release/net10.0/Individual_Project_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Individual_Project_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06be8a5693d88f1f6c154b63ac53d1533aded0a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4899fae4e405d3fe13c496c56d4103272a11ae0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Individual_Project_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Individual_Project_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
