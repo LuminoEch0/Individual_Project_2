6e7c7355061cb99ff0d9f25d1b066ba797402abd
jacks <jacks@LUMINOECHO>
2025-10-22 14:50:20 +0200

    Made some database test data in the index page and made the connection string with the database.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,13 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace Individual_Project_2.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public List<TestItem> TestItems { get; set; } = new List<TestItem>();
+
         public void OnGet()
         {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
 
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT Id, Name, Description FROM Test";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            TestItems.Add(new TestItem
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                                Description = reader.GetString(2)
+                            });
+                        }
+                    }
+                }
+            }
         }
+    }
+
+    public class TestItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
