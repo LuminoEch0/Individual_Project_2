f327abfec73770008ab7dc1e2ded84d581f05ae1
jacks <jacks@LUMINOECHO>
2025-11-04 13:32:05 +0100

    Made models to represent data from the database

---
+++ Models/Dashboard/MainDashboard/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Individual_Project_2.Models.Dashboard.MainDashboard
+{
+    public class BankAccount
+    {
+        public Guid AccountID { get; set; } //pk
+
+        public Guid UserID { get; set; } //fk - user
+
+        [Required]
+        [StringLength(100)]
+        public required string AccountName { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal CurrentBalance { get; private set; } = 0.00m;
+        public void UpdateBalance(decimal amount)
+        {
+            CurrentBalance += amount;
+        }
+    }
+}
---
+++ Models/Transaction/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using Individual_Project_2.Helpers.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Individual_Project_2.Models.Transaction
+{
+    public class Transaction
+    {
+        public Guid TransactionID { get; set; } // pk
+
+        public Guid CategoryID { get; set; } // fk - category
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Amount { get; set; }
+
+        [StringLength(256)]
+        public string? Description { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        public TransactionType Type { get; set; } // enum 0:expense, 1:income
+
+    }
+}
